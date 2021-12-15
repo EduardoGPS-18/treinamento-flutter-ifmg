03e6a63bcaf91d912d3a15ee2cdd869a894dc101
EduardoGPS-18 <eduardogps202@gmail.com>
2021-12-14 22:09:27 -0300

    refactor: criado a HomePageController

--- Flutter/requisicoes/lib/pages/home_page.dart
+++ Flutter/requisicoes/lib/pages/home_page.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:requisicoes/components/components.dart';
 
 import 'package:requisicoes/models/models.dart';
+import 'package:requisicoes/pages/pages.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,13 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<List<User>> get users async {
-    final usersMap = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users'));
-    await Future.delayed(const Duration(seconds: 2));
-    final body = jsonDecode(usersMap.body) as List;
-
-    return body.map((currentUserMap) => User.fromMap(currentUserMap as Map)).toList();
-  }
+  HomePageController homePageController = HomePageController();
 
   @override
   Widget build(BuildContext context) {
@@ -29,23 +22,24 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
       ),
       body: FutureBuilder<List<User>>(
-          future: users,
-          builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data != null) {
-              final users = snapshot.data!;
-              return ListView.builder(
-                itemCount: users.length,
-                itemBuilder: (ctx, idx) {
-                  final currentUser = users[idx];
-                  return UserTile(currentUser: currentUser);
-                },
-              );
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          }),
+        future: homePageController.users,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && snapshot.data != null) {
+            final users = snapshot.data!;
+            return ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (ctx, idx) {
+                final currentUser = users[idx];
+                return UserTile(currentUser: currentUser);
+              },
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
